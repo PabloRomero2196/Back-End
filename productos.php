c913684cd3d3cdeb8bfa1de2a64a4fd114bbe914
Pablo Romero <pabloandresromero@yahoo.com.mx>
2019-11-14 13:44:52 -0600

    vinculacion

--- productos.php
+++ productos.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Recetas</title>
+    <title>Productos</title>
     <link rel="stylesheet" href="styles.css">
     <script src="https://kit.fontawesome.com/54e3edb7eb.js" crossorigin="anonymous"></script>
 </head>
