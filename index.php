08838a10d05124979c7ccddde2f5c0580c4becc2
Pablo Romero <pabloandresromero@yahoo.com.mx>
2019-11-21 13:45:32 -0600

    tabs funcionales ! :)

--- index.php
+++ index.php
@@ -5,45 +5,36 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Inicio</title>
-    <link rel="stylesheet" href="styles.css">
+    <script src="script.js" defer></script>
+    <link href="styles.css" rel="stylesheet">
     <script src="https://kit.fontawesome.com/54e3edb7eb.js" crossorigin="anonymous"></script>
+    <title>Nuestra Casa</title>
 </head>
 
 <body>
 
     <?php include 'header.php'; ?>
 
+    <ul class="tabs">
+        <li data-tab-target="#home" class="active tab">Home</li>
+        <li data-tab-target="#pricing" class="tab">Pricing</li>
+        <li data-tab-target="#about" class="tab">About</li>
+    </ul>
 
-    <section class="recetas">
-        <h2>Pagina de Inicio</h2>
-        <!-- tabs -->
-        <ul class="tabs">
-            <li data-tab-target="#home" class="active tab">Home</li>
-            <li data-tab-target="#pricing" class="tab">Pricing</li>
-            <li data-tab-target="#about" class="tab">About</li>
-        </ul>
-
-        <!-- carrusel de imagenes -->
-        <div class="tab-content">
-            <div id="home" data-tab-content class="active">
-                <h1>Home</h1>
-                <p>This is the home</p>
-            </div>
-            <div id="pricing" data-tab-content>
-                <h1>Pricing</h1>
-                <p>Some information on pricing</p>
-            </div>
-            <div id="about" data-tab-content>
-                <h1>About</h1>
-                <p>Let me tell you about me</p>
-            </div>
+    <div class="tab-content">
+        <div id="home" data-tab-content class="active">
+            <h1>Home</h1>
+            <p>This is the home</p>
         </div>
-
-
-
-
-    </section>
+        <div id="pricing" data-tab-content>
+            <h1>Pricing</h1>
+            <p>Some information on pricing</p>
+        </div>
+        <div id="about" data-tab-content>
+            <h1>About</h1>
+            <p>Let me tell you about me</p>
+        </div>
+    </div>
 </body>
 
 </html>
